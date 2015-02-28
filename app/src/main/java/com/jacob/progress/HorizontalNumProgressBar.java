7eff94e079f3b530f5599a79eeed8bac69043b80
王佳 <wangjia@cvte.cn>
2015-02-28 12:09:46 +0800

    完成水平自定义进度条

---
+++ app/src/main/java/com/jacob/progress/HorizontalNumProgressBar.java
@@ -0,0 +1,130 @@
+package com.jacob.progress;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.NonNull;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.widget.ProgressBar;
+
+/**
+ * Package : com.jacob.progress
+ * Author : jacob
+ * Date : 15-2-28
+ * Description : 这个类是用来xxx
+ */
+public class HorizontalNumProgressBar extends ProgressBar {
+    public static final int PROGRESS_REACHED_COLOR = 0xFF4587FF;
+    public static final int PROGRESS_UNREACHED_COLOR = 0xFF696969;
+    public static final int PROGRESS_HEIGHT =4;
+    public static final int PROGRESS_TEXT_COLOR = 0XFFFC00D1;
+    public static final int PROGRESS_TEXT_SIZE = 10;
+    public static final int PROGRESS_TEXT_OFFSET = 5;
+
+    private int progressReachedColor = PROGRESS_REACHED_COLOR;
+    private int progressUnReachedColor = PROGRESS_UNREACHED_COLOR;
+    private int progressHeight = PROGRESS_HEIGHT;
+    private int progressTextColor = PROGRESS_TEXT_COLOR;
+    private int progressTextSize = PROGRESS_TEXT_SIZE;
+    private int progressTextOffset = PROGRESS_TEXT_OFFSET;
+
+    private Paint mPaint = new Paint();
+    private int realW;
+
+    public HorizontalNumProgressBar(Context context) {
+        this(context, null);
+    }
+
+    public HorizontalNumProgressBar(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public HorizontalNumProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.HorizontalNumProgressBar);
+        progressHeight = (int) array.getDimension(R.styleable.HorizontalNumProgressBar_progress_height, dpTopx(PROGRESS_HEIGHT));
+        progressReachedColor = array.getColor(R.styleable.HorizontalNumProgressBar_progress_reached_color, PROGRESS_REACHED_COLOR);
+        progressUnReachedColor = array.getColor(R.styleable.HorizontalNumProgressBar_progress_unreached_color, PROGRESS_UNREACHED_COLOR);
+        progressTextColor = array.getColor(R.styleable.HorizontalNumProgressBar_progress_text_color, PROGRESS_TEXT_COLOR);
+        progressTextSize = (int) array.getDimension(R.styleable.HorizontalNumProgressBar_progress_text_size, dpTopx(PROGRESS_TEXT_SIZE));
+        progressTextOffset = (int) array.getDimension(R.styleable.HorizontalNumProgressBar_progress_text_offset, dpTopx(PROGRESS_TEXT_OFFSET));
+        array.recycle();
+
+        mPaint.setAntiAlias(true);
+        mPaint.setTextSize(progressTextSize);
+    }
+
+
+    @Override
+    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        realW = w - getPaddingLeft() - getPaddingRight();
+    }
+
+    @Override
+    protected synchronized void onDraw(@NonNull Canvas canvas) {
+        boolean drawUnreached = true;
+        canvas.save();
+        canvas.translate(getPaddingLeft(), getHeight() / 2);
+        String text = getProgress() + "%";
+        float textW = mPaint.measureText(text);
+        float textH = (mPaint.descent() + mPaint.ascent()) / 2;
+
+        float leftLineW = (float) (realW * (getProgress() * 1.0 / getMax())) - textW - progressTextOffset*2 ;
+        float rightLineW = (float) (realW * (getProgress() * 1.0 / getMax()));
+        float centerH = (getMeasuredHeight() - progressHeight) / 2 ;
+
+        if (leftLineW + textW > realW) {
+            leftLineW = realW - textW;
+            drawUnreached = false;
+        }
+
+        int strokW = progressHeight;
+        mPaint.setColor(progressReachedColor);
+        mPaint.setStrokeWidth(strokW);
+        mPaint.setAntiAlias(true);
+        canvas.drawLine(0, centerH, leftLineW, centerH, mPaint);
+
+
+        mPaint.reset();
+        mPaint.setColor(progressTextColor);
+        mPaint.setAntiAlias(true);
+        mPaint.setTextSize(progressTextSize);
+        canvas.drawText(text, leftLineW , (getMeasuredHeight() - textH) / 2, mPaint);
+
+        if (drawUnreached) {
+            mPaint.reset();
+            mPaint.setColor(progressUnReachedColor);
+            mPaint.setStrokeWidth(strokW);
+            mPaint.setAntiAlias(true);
+            canvas.drawLine(rightLineW, centerH, realW, centerH, mPaint);
+        }
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public synchronized void setProgress(int progress) {
+        super.setProgress(progress);
+    }
+
+    private int dpTopx(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
+    }
+//
+//    private int spToPx(int sp) {
+//        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, getResources().getDisplayMetrics());
+//    }
+}
--- app/src/main/java/com/jacob/progress/MainActivity.java
+++ app/src/main/java/com/jacob/progress/MainActivity.java
@@ -1,39 +1,34 @@
 package com.jacob.progress;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
+    HorizontalNumProgressBar horizontalNumProgressBar;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (horizontalNumProgressBar.getProgress() >= 100) {
+                removeMessages(10);
+                return;
+            }
+            sendEmptyMessageDelayed(10, 80);
+            horizontalNumProgressBar.setProgress(horizontalNumProgressBar.getProgress() + 1);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        horizontalNumProgressBar = (HorizontalNumProgressBar) findViewById(R.id.progress_horizontal);
+        mHandler.sendEmptyMessage(10);
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
